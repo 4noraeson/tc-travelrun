6aecb85217cfea3ab9112b4d4ee62225a64b1d3d
maraguida@gmail.com <maraguida@gmail.com@aaaadec0-1659-87bb-47ea-f6a435eb4496>
2013-12-01 13:16:53 +0000

    keywords

---
+++ public_html/manupdate2.php
@@ -0,0 +1,54 @@
+<?php
+## $Id$
+## $URL$
+
+require '.config.php';
+
+if ($_SERVER['REQUEST_METHOD'] != 'POST') die('No!');
+
+$country = array(260=>1, 617=>2, 263=>3, 264=>4, 267=>5, 271=>6, 272=>7, 277=>8, 276=>9, 385=>10, 282=>11);
+
+$flower = 1 * $_POST['flower'];
+
+if ($flower && isset($country[$flower]) && (trim($_POST['gmt']) != '') && (trim($_POST['qtd']) != '')) {
+  if (preg_match('@(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) \d\d, \d\d\d\d \d\d:\d\d@', $_POST['time'], $matches) == 1) {
+    $flowertime = strtotime($matches[0]);
+    $gmtoffset = 1 * $_POST['gmt'];
+    $unixtime = date('Y-m-d H:i:s', $flowertime - 60*60*$gmtoffset);
+    $quantity = 1 * $_POST['qtd'];
+    $sender = 'web';
+    if (isset($_POST['sender'])) $sender = $_POST['sender'];
+    $safesender = mysql_real_escape_string($sender);
+
+    // open the database connection
+    $conn = mysql_connect(SQL_HOST, SQL_USER, SQL_PASS) or die(mysql_error());
+    mysql_select_db(SQL_DATA);
+
+    $sql = "insert into stock (utctime, country, item, price, quantity, manual, sender) values ('$unixtime', $country[$flower], $flower, 0, $quantity, 1, '$safesender')";
+    mysql_query($sql) or die(mysql_error());
+
+    // close the database connection
+    mysql_close($conn);
+
+    # delete image files
+    $files = glob('images/*');
+    if ($files) {
+      foreach ($files as $file) {
+        if (is_file($file)) unlink($file);
+      }
+    }
+
+    echo '<b>Flower updated. The data will only be used to draw dots on the graph.</b><br>';
+    echo '<br>';
+    echo '<a href="manupdate.php">Update another</a> or return to the <a href="default.php">start page</a>.';
+  } else {
+    echo '<b>Update failed.</b><br>';
+    echo '<br>';
+    echo '<a href="manupdate.php">Try updating again</a> or return to the <a href="default.php">start page</a>.';
+  }
+} else {
+  echo '<b>Unable to extract time.</b><br>';
+  echo '<br>';
+  echo '<a href="manupdate.php">Try updating again</a> or return to the <a href="default.php">start page</a>.';
+}
+?>
