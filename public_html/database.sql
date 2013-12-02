d614ea1b877243da4cb187a34c491b44082bcf24
maraguida@gmail.com <maraguida@gmail.com@aaaadec0-1659-87bb-47ea-f6a435eb4496>
2013-12-02 12:27:28 +0000

    added structure for table stock

--- public_html/database.sql
+++ public_html/database.sql
@@ -801,3 +801,24 @@ CREATE TABLE post (
   sender varchar(78) COLLATE latin1_general_ci NOT NULL DEFAULT ' ',
   PRIMARY KEY (postid)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=4023 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table 'stock'
+--
+
+CREATE TABLE stock (
+  stockid int(11) NOT NULL AUTO_INCREMENT,
+  utctime datetime NOT NULL,
+  country int(11) NOT NULL,
+  item int(11) NOT NULL,
+  price int(11) NOT NULL,
+  quantity int(11) NOT NULL,
+  manual tinyint(4) NOT NULL,
+  sender varchar(78) COLLATE latin1_general_ci NOT NULL,
+  PRIMARY KEY (stockid),
+  KEY country (country),
+  KEY item (item)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=51013 ;
+
