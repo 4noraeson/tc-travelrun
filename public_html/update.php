d5d0f67291e4501c866044b254c5d2418df123e8
maraguida@gmail.com <maraguida@gmail.com@aaaadec0-1659-87bb-47ea-f6a435eb4496>
2013-12-03 10:08:21 +0000

    issue #4 solved, added another update button

--- public_html/update.php
+++ public_html/update.php
@@ -19,7 +19,8 @@ Go back to the <a href="default.php">start page</a>.<br>
   <!--font size="-2">(if you're paranoid, you can edit the amount you have to,
   for example, <b>$100</b>, but leave that line in with no other changes)</font--><br>
   and click the "UPDATE" button<br>
-  <i>The pasted text has to contain the line with the country name and your money; you can safely edit the amount of money but leave that line otherwise unchanged</i><br><br>
+  <i>The pasted text has to contain the line with the country name and your money; you can safely edit the amount of money but leave that line otherwise unchanged</i><br>
+  <br><input type="submit" value="UPDATE"><br>
   <textarea name="data" rows="20" cols="80"></textarea>
   <br><input type="submit" value="UPDATE"> <i>Thank you.</i>
 </form>
