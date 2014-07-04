c289b705d0fb4bc38f09773a8a5cdab5e6371fa9
maraguida@gmail.com <maraguida@gmail.com@aaaadec0-1659-87bb-47ea-f6a435eb4496>
2014-07-04 09:24:14 +0000

    prices

--- public_html/itemall.php
+++ public_html/itemall.php
@@ -18,7 +18,7 @@ mysql_select_db(SQL_DATA);
 // get the data
 $itemall = array();
 $sql = <<<SQL_ITEMALL
-select s.utctime, country.countryname, item.itemname, s.quantity
+select s.utctime, country.countryname, item.itemname, s.quantity, s.price
 
 from stock s
    , (select max(utctime) as mt, country, item from stock where manual = 0 group by country, item) smax
@@ -36,20 +36,21 @@ order by 1 desc, 2, 3
 SQL_ITEMALL;
 $res = mysql_query($sql) or die(mysql_error());
 while ($row = mysql_fetch_row($res)) {
-  $itemall[] = array($row[0], $row[1], $row[2], $row[3]);
+  $itemall[] = array($row[0], $row[1], $row[2], $row[3], $row[4]);
 }
 mysql_free_result($res);
 mysql_close($conn);
 
 echo '<div class="itemalldata">';
 echo '<table border="1">';
-echo '<tr><th>date</th><th>country</th><th>item</th><th>quantity</th></tr>';
+echo '<tr><th>date</th><th>country</th><th>item</th><th>quantity</th><th>price</th></tr>';
 foreach ($itemall as $d) {
   echo '<tr>';
   echo '<td>&nbsp;', $d[0], '&nbsp;</td>';
   echo '<td>&nbsp;', $d[1], '&nbsp;</td>';
   echo '<td>&nbsp;', $d[2], '&nbsp;</td>';
   echo '<td align="right">&nbsp;', $d[3], '&nbsp;</td>';
+  echo '<td align="right">&nbsp;', $d[4], '&nbsp;</td>';
   echo '</tr>';
 }
 echo '</table>';
