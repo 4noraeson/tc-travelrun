2d8ff4aca3b32a5aab3afc0a8dc22d3b94c33887
maraguida@gmail.com <maraguida@gmail.com@aaaadec0-1659-87bb-47ea-f6a435eb4496>
2014-12-09 19:20:30 +0000

    RX error

--- public_html/update2.php
+++ public_html/update2.php
@@ -64,7 +64,7 @@ if ($n == 1) {
     $t = array('Alcohol', 'Defensive', 'Drug', 'Enhancer', 'Flower', 'Medical', 'Melee', 'Other', 'Plushie', 'Primary', 'Secondary', 'Temporary');
     $rxt = implode('|', $t);
     #$rx = '@(?:' . $rxt . ')(?:\s+x[0-9,]+)?\s+([0-9A-Z &a-z-]+)\s+\$([0-9,]+)\s+([0-9,]+)@';
-    $rx = '@Type:\s+(?:' . $rxt . ')\s+(?:x[0-9,]+\s+)?([0-9A-Z &a-z-]+)\s+Cost:\s+\$([0-9,]+)\s+In\s+stock:\s+([0-9,]+)@';
+    $rx = '@Type:\s+(?:' . $rxt . ')\s+(?:x[0-9,]+\s+)?([0-9A-Z &a-z-]+)\s+Cost:\s+\$([0-9,]+)\s+In\s+stock:\s*([0-9,]+)@';
     if (preg_match_all($rx, $_POST['data'], $matches, PREG_SET_ORDER)) {
       foreach ($matches as $info) {
         $itemname = trim($info[1]);
@@ -82,7 +82,7 @@ if ($n == 1) {
         }
         if (preg_match('/ Plushie/', $itemname)) {
           $gotplushie = 1;
-          $_SESSION['recent_update']['plushie'] .= $itemleft . ' ' $itemname . '<br>';
+          $_SESSION['recent_update']['plushie'] .= $itemleft . ' ' . $itemname . '<br>';
         }
       }
     }
